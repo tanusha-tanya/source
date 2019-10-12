5aaf7f8f62321d1292a2cb2eac47c64ed184d1b7
Tanya <offchinnikova@bk.ru>
2019-10-13 00:58:57 +0400

    history

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,3 @@
 <template>
-    <div class="body">
-        <header class="header">
-            
-        </header>
-        <main class="main"></main>
-        <footer class="footer"></footer>
-    </div>
 </template>
+
